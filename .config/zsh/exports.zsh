1835ff715d85fb30101cc5857700db07cbe72712
github username <github email address>
2022-05-03 00:39:45 -0400

    fixed export locations

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -8,7 +8,8 @@ plugins=(
   zsh-syntax-highlighting
   zsh-autosuggestions
 )
-export ZSH="/home/$USER/.oh-my-zsh"
+# export ZSH="/home/$USER/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 source $ZSH/oh-my-zsh.sh
 
 # export
@@ -26,9 +27,18 @@ export FZF_DEFAULT_COMMAND='rg --files --hidden'
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
-export PATH=/home/$USER/.cargo/bin:$PATH
-export PATH=/home/$USER/.local/go/bin:$PATH
-export GOPATH=$HOME/.local/go
 
-# fnm
-# eval "$(zoxide init zsh)"
+export GOROOT=/usr/local/go
+# export GOROOT=/usr/lib/go
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=~/Development/go_code
+export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.local/bin/statusbar:$PATH
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
--- .zshrc
+++ .zshrc
@@ -9,19 +9,5 @@
 [ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
 [ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
 
-# export GOROOT=/usr/local/go
-export GOROOT=/usr/lib/go
-export PATH=$PATH:/usr/lib/go/bin
-export GOPATH=~/Development/go
-export PATH=$HOME/.local/bin:$PATH
-export PATH=$HOME/.local/bin/statusbar:$PATH
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-source ~/.rvm/scripts/rvm
+# source ~/.rvm/scripts/rvm
 
